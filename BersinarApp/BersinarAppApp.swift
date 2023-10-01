a6ab0b04c679ef23e8a5e7479c0318cd170dcc4e
Andira Yunita <andhirayp@gmail.com>
2023-10-01 16:51:37 +0700

    Initial Commit

---
+++ BersinarApp/BersinarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BersinarAppApp.swift
+//  BersinarApp
+//
+//  Created by Andira Yunita on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BersinarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
